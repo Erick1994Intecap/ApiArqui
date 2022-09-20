624013321fe287291f113c7da5a5a5ad3888f827
Er <erickruiz1994@gmail.com>
2022-09-19 23:40:26 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication5/Controllers/ClienteController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication5.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication5.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        static List<Cliente> clientes = new List<Cliente>();
+        // GET: api/<ClienteController>
+        [HttpGet]
+        public ActionResult<List<Cliente>> Get()
+        {
+            if (clientes.Count == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(clientes);
+            }
+            
+        }
+
+        // POST api/<ClienteController>
+        [HttpPost]
+        public void Post([FromBody] Cliente cliente)
+        {
+            clientes.Add(cliente);
+        }
+
+       
+    }
+}
---
+++ WebApplication5/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication5.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
